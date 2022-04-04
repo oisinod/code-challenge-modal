bb07879b9c18deefa5f4e6a9855001d92a1d2cbd
oisin odoherty <oisin.odoherty@ucdconnect.ie>
2022-04-04 17:07:01 +0200

    added formatting to email as per email client conventions

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.0].define(version: 2022_04_01_100626) do
+ActiveRecord::Schema[7.0].define(version: 2022_04_04_144211) do
   create_table "cycles", force: :cascade do |t|
     t.string "name"
     t.datetime "created_at", null: false
@@ -24,6 +24,7 @@ ActiveRecord::Schema[7.0].define(version: 2022_04_01_100626) do
     t.integer "cycle_id", null: false
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.string "name"
     t.index ["cycle_id"], name: "index_invitations_on_cycle_id"
   end
 
